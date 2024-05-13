55b4049542513706e7247b207c2431498ea149c1
josuembuyu6 <josuembuyu94@gmail.com>
2024-05-13 17:04:46 +0100

    Create account api and integration

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
 import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-  return <div></div>;
+  const { onOpen } = useNewAccount();
+
+  return (
+    <div>
+      <Button onClick={onOpen}>Add an account</Button>
+    </div>
+  );
 }
---
+++ features/accounts/components/new-account-sheet.tsx
@@ -0,0 +1,56 @@
+import { z } from "zod";
+
+import { insertAccountSchema } from "@/db/schema";
+
+import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { useCreateAccount } from "@/features/accounts/api/use-create-account";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { AccountForm } from "./account-form";
+
+export const NewAccountSheet = () => {
+  const { isOpen, onClose } = useNewAccount();
+
+  const mutation = useCreateAccount();
+
+  const formSchema = insertAccountSchema.pick({
+    name: true,
+  });
+
+  type FormValues = z.input<typeof formSchema>;
+
+  const onSubmit = (values: FormValues) => {
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
+  };
+
+  return (
+    <Sheet open={isOpen} onOpenChange={onClose}>
+      <SheetContent className="space-y-4">
+        <SheetHeader>
+          <SheetTitle>New Account</SheetTitle>
+          <SheetDescription>
+            Create a new account to track your transactions.
+          </SheetDescription>
+        </SheetHeader>
+
+        <AccountForm
+          onSubmit={onSubmit}
+          disabled={mutation.isPending}
+          defaultValues={{
+            name: "",
+          }}
+        />
+      </SheetContent>
+    </Sheet>
+  );
+};
